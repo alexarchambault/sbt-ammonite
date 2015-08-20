958812ab777833ca6b5ec627d38646e348e70dd9
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-08-20 02:45:04 +0100

    Add support for initialCommands

--- src/main/scala/ammonite/AmmonitePlugin.scala
+++ src/main/scala/ammonite/AmmonitePlugin.scala
@@ -1,5 +1,7 @@
 package ammonite
 
+import sbt.Attributed._
+import sbt.Project.Initialize
 import sbt._, Keys._
 
 object AmmonitePlugin extends AutoPlugin {
@@ -19,6 +21,29 @@ object AmmonitePlugin extends AutoPlugin {
   override lazy val projectSettings =
     ammoniteSettings(Ammonite, Compile) ++
     ammoniteSettings(AmmoniteTest, Test)
+
+  // Same as Defaults.runTask from SBT, but accepting default arguments too
+  def runTask(
+    classpath: Initialize[Task[Classpath]],
+    mainClassTask: Initialize[Task[Option[String]]],
+    scalaRun: Initialize[Task[ScalaRun]],
+    defaultArgs: Initialize[Task[Seq[String]]]
+  ): Initialize[InputTask[Unit]] = {
+    import Def.parserToInput
+    val parser = Def.spaceDelimited()
+    Def.inputTask {
+      val mainClass = mainClassTask.value getOrElse sys.error("No main class detected.")
+      val userArgs = parser.parsed
+      val args = if (userArgs.isEmpty) defaultArgs.value else userArgs
+      toError(scalaRun.value.run(mainClass, data(classpath.value), args, streams.value.log))
+    }
+  }
+
+  def defaultArgs(initialCommands: String): Seq[String] =
+    if (initialCommands.isEmpty)
+      Nil
+    else
+      Seq("--predef", initialCommands)
 
   def ammoniteSettings(ammoniteConf: Configuration, underlyingConf: Configuration) = inConfig(ammoniteConf)(
     // Getting references to undefined settings when doing ammonite:run without these
@@ -40,8 +65,9 @@ object AmmonitePlugin extends AutoPlugin {
       configuration := underlyingConf,
 
       /* Overriding run and runMain defined by compileSettings so that they use fullClasspath of this scope (Ammonite),
-       * taking into account the extra libraryDependencies above. */
-      run <<= Defaults.runTask(fullClasspath, mainClass in run, runner in run),
+       * taking into account the extra libraryDependencies above, and we can also supply default arguments
+       * (initialCommands as predef). */
+      run <<= runTask(fullClasspath, mainClass in run, runner in run, (initialCommands in console).map(defaultArgs)),
       runMain <<= Defaults.runMainTask(fullClasspath, runner in run),
 
       mainClass := Some("ammonite.repl.Repl"),
